66b3d52166235770fef327573c1a0c9cd129e696
微软 张 <188140725@qq.com>
2020-11-21 14:26:27 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Button>
+      <router-link to="/login"> 登录 </router-link>
+    </Button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { Button } from "ant-design-vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Button
   }
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="login">
+  <Form
+    :model="loginInput"
+    :labelCol="{ span: 4 }"
+    :wrapperCol="{ span: 14 }"
+    @submit="handleSubmit"
+    @submit.prevent
+  >
+    <Form.Item :label="'Username'">
+      <Input v-model:value="loginInput.username" placeholder="Username" />
+    </Form.Item>
+    <Form.Item label="Password">
+      <Input
+        v-model:value="loginInput.password"
+        placeholder="Password"
+        type="password"
+      />
+    </Form.Item>
+    <Form.Item>
+      <Button
+        type="primary"
+        html-type="submit"
+        :disabled="loginInput.username === '' || loginInput.password === ''"
+      >
+        Log in
+      </Button>
+    </Form.Item>
+  </Form></div>
+</template>
+<script>
+import { Input, Button,Form } from "ant-design-vue";
+export default {
+  data() {
+    return {
+      loginInput: {
+        username: "12",
+        password: "12",
+      },
+    };
+  },
+  components: { Input, Button,Form },
+  methods: {
+    handleSubmit(e) {
+      console.log(e, this.loginInput);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.login{
+    position: absolute;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+}
+</style>
